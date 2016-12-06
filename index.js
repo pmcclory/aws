2a2f0b1cb182f838868982bc85da5a403eeaad91
Andrew Winder <andrew.winder@sparkpost.com>
2016-12-06 13:37:08 -0500

    singleton dynamo instance + config system changes

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+const cache = require('./lib/cache');
+
+let initialized = false;
+
 module.exports = {
+  isInitialized: () => {
+    return initialized;
+  },
+  initialize: (conf) => {
+    let awsConf = {}
+      , configurationKeys = ['accessKeyId', 'secretAccessKey', 'region', 'httpOptions'];
+
+    configurationKeys.forEach((key) => {
+      if (key in conf) {
+        awsConf[key] = conf[key];
+      }
+    });
+
+    AWS.config.update(awsConf);
+    cache.set('awsInitialized', true);
+  },
   DynamoDB: require('./lib/dynamo')
 };
